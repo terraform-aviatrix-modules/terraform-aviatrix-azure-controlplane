8d8e2aeb277a835b1179ca52fc5f34d18bcb2981
Dennizz <root@ipaddr.nl>
2025-01-08 11:28:00 +0100

    bump copilot init to 1.0.4

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "copilot_init" {
   count = var.module_config.copilot_initialization ? 1 : 0
 
   source  = "terraform-aviatrix-modules/copilot-init/aviatrix"
-  version = "v1.0.3"
+  version = "v1.0.4"
 
   controller_public_ip             = module.controller_build[0].controller_public_ip_address
   controller_admin_password        = var.controller_admin_password
